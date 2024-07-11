39baaf0995b9f54b2c4c5e5dc207cd6d5423aebb
HaraldRichter <richter.harald@proton.me>
2024-07-11 12:11:41 +0200

    Added Buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,7 +13,8 @@ public class Calculator implements ActionListener {
     JButton decButton, equButton, delButton, clrButton;
     JPanel panel;
 
-    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+    // Font myFont = new Font("Ink Free", Font.BOLD, 30);
+    Font myFont = new Font("Monospaced", Font.BOLD, 30);
 
     double num1 = 0, num2 = 0, result = 0;
     char operator;
@@ -29,6 +30,73 @@ public class Calculator implements ActionListener {
         textField.setFont(myFont);
         textField.setEditable(false);
 
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = delButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        // The loop iterates over the function-buttons to add functionality to each of them.
+        for (int i = 0; i < 8; i++) {
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        // This loop iterates over the number-buttons
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i)); // automatically provides the numbers to our buttons
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
+
+        // We declare a panel for our buttons with a 4x4 grid layout:
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+        // panel.setBackground(Color.GRAY);
+
+        // The delete- and clear-buttons aren't part of the grid, so they are built separately here:
+        delButton.setBounds(50, 430, 145, 50);
+        clrButton.setBounds(205, 430, 145, 50);
+
+        // We add the buttons to the panel in the desired order.
+        // The buttons are placed starting with the upper left corner to the bottom right corner.
+        // Each row has 4 buttons, as we specified the panel to be a 4x4 grid.
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+        // Here the components are finally added to the frame:
+        frame.add(panel);
+        frame.add(delButton);
+        frame.add(clrButton);
         frame.add(textField);
         frame.setVisible(true);
     }
